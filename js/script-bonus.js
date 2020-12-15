a9744809c916721f1b129fdca7abbe9b5aa67fa8
Salvatore Napoli <snapoli1998@gmail.com>
2020-12-15 19:15:57 +0100

    Sistemata versione classica e aggiunto bonus

--- js/script-bonus.js
+++ js/script-bonus.js
@@ -1,6 +1,8 @@
-// Verifica indirizzo email
+// Verifica indirizzo email e password
 var mail = prompt('Qual è il tuo indirizzo email?');
+var password = prompt('Qual è la password?');
 var whitelist = ['mariorossi@gmail.com', 'ciao@hotmail.it', 'ok@libero.it']; // Lista di indirizzi consentiti
+var correctPassword = 'lanciadadi';
 var mailOutput = document.getElementById('mail-output');
 var diceGame = document.getElementById('dice-game');
 var mailVerified = false;
@@ -10,7 +12,7 @@ mailOutput.innerText = 'Scusa, non hai i permessi necessari ad accedere al gioco
 mailOutput.className = 'text-red text-bold text-center font-30';
 
 for (var i = 0; i < whitelist.length; i++) {
-  if (mail === whitelist[i]) {
+  if (mail === whitelist[i] && password === correctPassword) {
     mailVerified = true;
   }
 }
@@ -23,7 +25,7 @@ if (mailVerified) {
 
 // // Versione alternativa
 // var checkMail = whitelist.includes(mail);
-// if (checkMail === true) {
+// if (checkMail === true && password === correctPassword) {
 //   mailOutput.innerText = 'Complimenti! Puoi accedere al gioco.';
 //   mailOutput.className = 'text-green text-bold text-center font-30';
 //   diceGame.style.display = 'block';
@@ -35,24 +37,20 @@ if (mailVerified) {
 
 // Gioco dei dadi
 var yourDice = document.getElementById('your-dice');
-var yourResult = document.getElementById('your-result');
-var opponentResult = document.getElementById('opponent-result');
+var yourResults = document.getElementsByClassName('your-result');
+var opponentResults = document.getElementsByClassName('opponent-result');
 var gameResult = document.getElementById('game-result');
 
 document.getElementsByTagName('img')[0].addEventListener('click', function rollDice () {
   var min, max, yourDiceResult, opponentDiceResult;
   min = 1;
   max = 6;
-  yourDiceResult = Math.floor(Math.random() * (max + 1 - min) + min);
-  opponentDiceResult = Math.floor(Math.random() * (max + 1 - min) + min);
-  yourResult.innerText = yourDiceResult;
-  opponentResult.innerText = opponentDiceResult;
-  if (yourDiceResult > opponentDiceResult) {
-    gameResult.innerText = 'VITTORIA';
-  } else if (yourDiceResult < opponentDiceResult) {
-    gameResult.innerText = 'SCONFITTA';
-  } else {
-    gameResult.innerText = 'PAREGGIO';
+
+  for (var i = 0; i < yourResults.length; i++) {
+    yourDiceResult = Math.floor(Math.random() * (max + 1 - min) + min);
+    yourResults[i].innerText = yourDiceResult;
+    opponentDiceResult = Math.floor(Math.random() * (max + 1 - min) + min);
+    opponentResults[i].innerText = opponentDiceResult;
   }
 }
 )
